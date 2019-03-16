ba09bc4fbd14071e5a736fc427b8b7de2da04a07
Kristijan Lečnik <janez3k@gmail.com>
2019-03-16 23:27:34 +0100

    expand list files to scan subfolders and add the files to the list

--- app/io.js
+++ app/io.js
@@ -2,35 +2,56 @@ var fs = require('fs');
 var exec = require('child_process').exec;
 var config = require('./config.json');
 
+var walk    = require('walk');
+global.files   = [];
+
+function listFiles()
+{
+	global.files   = [];
+
+	var options;
+	var walker;
+
+	options = {
+		listeners: {
+			file: function(root, stat, next) {
+				var tmp = root + '/' + stat.name; 
+				global.files.push(tmp.replace(config.config_folder, ''));
+				next();
+			}
+		}
+	};
+
+	walker = walk.walkSync(config.config_folder, options);
+
+}
+
 module.exports = function (io) {
   io.on('connection', function (client) {
     console.log(client.request.connection.remoteAddress + " Client connected");
-    fs.readdir(config.config_folder, function(err, files) {
-      io.emit('list-configs', files);
-    });
+    listFiles()
+    io.emit('list-configs', files);
 
     client.on('show-config', function(file) {
-      fs.readFile(config.config_folder + '/' + file, 'utf8', function(err, data) {
+      fs.readFile(config.config_folder + file, 'utf8', function(err, data) {
         var obj = {'file': file, 'data': data};
         io.emit('show-config', obj);
       });
     });
 
     client.on('save-config', function (obj) {
-      fs.writeFile(config.config_folder + '/' + obj.file, obj.data, function (err) {
+      fs.writeFile(config.config_folder + obj.file, obj.data, function (err) {
         console.log(client.request.connection.remoteAddress + " config saved - " + obj.file);
-        fs.readdir(config.config_folder, function (err, files) {
-          io.emit('list-configs', files);
-        });
+        listFiles()
+        io.emit('list-configs', files);
       });
     });
 
     client.on('delete-config', function (file) {
-      fs.unlink(config.config_folder + '/' + file, function() {
-        console.log(client.request.connection.remoteAddress + " config deleted");
-        fs.readdir(config.config_folder, function (err, files) {
-          io.emit('list-configs', files);
-        });
+      fs.unlink(config.config_folder + file, function() {
+        console.log(client.request.connection.remoteAddress + " config deleted - " + file);
+        listFiles()
+        io.emit('list-configs', files);
       });
     });
 
