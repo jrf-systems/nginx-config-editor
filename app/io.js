6f93580ada40332a34976f02faedf8825adde85e
liupo.wu <liupo.wu@wz-inc.com>
2020-08-31 10:56:45 +0800

    feat: use dynamic config

--- app/io.js
+++ app/io.js
@@ -1,96 +1,96 @@
 var fs = require('fs');
 var exec = require('child_process').exec;
-var configpath = __dirname + '/config.json'
+var configpath = __dirname + '/config.json';
 var config = require(configpath);
 var path = require('path');
 
-var walk    = require('walk');
-global.files   = [];
+var walk = require('walk');
+global.files = [];
 
-function listFiles()
-{
-	global.files   = [];
+var nginx = process.env.NGINX || 'nginx';
+var configFoler = process.env.CONFIG || config.config_folder;
 
-	var options;
-	var walker;
+function listFiles() {
+  global.files = [];
 
-	options = {
-		listeners: {
-			file: function(root, stat, next) {
-				var tmp = path.join(root, stat.name); 
-				global.files.push(tmp.replace(config.config_folder, '').replace('/',''));
-				next();
-			}
-		}
-	};
+  var options;
+  var walker;
 
-	walker = walk.walkSync(config.config_folder, options);
+  options = {
+    listeners: {
+      file: function (root, stat, next) {
+        var tmp = path.join(root, stat.name);
+        global.files.push(tmp.replace(configFoler, '').replace('/', ''));
+        next();
+      },
+    },
+  };
 
+  walker = walk.walkSync(configFoler, options);
 }
 
 module.exports = function (io) {
   io.on('connection', function (client) {
-    console.log(client.request.connection.remoteAddress + " Client connected");
-    listFiles()
+    console.log(client.request.connection.remoteAddress + ' Client connected');
+    listFiles();
     io.emit('list-configs', files);
 
-    client.on('show-config', function(file) {
-      fs.readFile(path.join(config.config_folder, file), 'utf8', function(err, data) {
-        var obj = {'file': file, 'data': data};
+    client.on('show-config', function (file) {
+      fs.readFile(path.join(configFoler, file), 'utf8', function (err, data) {
+        var obj = { file: file, data: data };
         io.emit('show-config', obj);
       });
     });
 
     client.on('save-config', function (obj) {
       var title = obj.file;
-      var filename = "";
+      var filename = '';
       if (title == configpath) {
-        filename = configpath
-      }
-      else {
-        filename = path.join(config.config_folder, obj.file);
+        filename = configpath;
+      } else {
+        filename = path.join(configFoler, obj.file);
       }
       fs.writeFile(filename, obj.data, function (err) {
-        console.log(client.request.connection.remoteAddress + " config saved - " + obj.file);
-        listFiles()
+        console.log(client.request.connection.remoteAddress + ' config saved - ' + obj.file);
+        listFiles();
         io.emit('list-configs', files);
       });
     });
 
     client.on('delete-config', function (file) {
-      fs.unlink(config.config_folder + file, function() {
-        console.log(client.request.connection.remoteAddress + " config deleted - " + file);
-        listFiles()
+      fs.unlink(configFoler + file, function () {
+        console.log(client.request.connection.remoteAddress + ' config deleted - ' + file);
+        listFiles();
         io.emit('list-configs', files);
       });
     });
 
     client.on('reload-nginx', function (action) {
-      var command = "nginx -s reload";
+      var command = `${nginx} -s reload`;
       exec(command, function (err, stdout, stderr) {
         if (err) {
           io.emit('error', err.toString());
         } else {
           io.emit('reload-success', stdout);
-          console.log(client.request.connection.remoteAddress + " reload-success");
+          console.log(client.request.connection.remoteAddress + ' reload-success');
         }
       });
     });
 
     client.on('restart-nginx', function (action) {
-      var command = "systemctl restart nginx";
+      var command = `${nginx} -s stop && ${nginx}`; // "systemctl restart nginx";
       exec(command, function (err, stdout, stderr) {
         if (err) {
           io.emit('error', err.toString());
         } else {
           io.emit('restart-success', stdout);
-          console.log(client.request.connection.remoteAddress + " restart-success");
+          console.log(client.request.connection.remoteAddress + ' restart-success');
         }
       });
     });
 
     client.on('check-syntax', function (action) {
-      var command = "nginx -t";
+      var command = `${nginx} -t`;
       exec(command, function (err, stdout, stderr) {
         if (err) {
           io.emit('error', err.toString());
@@ -101,10 +101,10 @@ module.exports = function (io) {
     });
 
     client.on('load-config', function (action) {
-      fs.readFile(configpath, 'utf8', function(err, data) {
-        var obj = {'file': configpath, 'data': data};
+      fs.readFile(configpath, 'utf8', function (err, data) {
+        var obj = { file: configpath, data: data };
         io.emit('load-config', obj);
       });
     });
   });
-}
+};
